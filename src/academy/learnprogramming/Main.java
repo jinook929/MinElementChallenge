1517a907d63e9fb4759c57add76c910728bf9246
jinook929 <jinook929@gmail.com>
2019-11-07 21:54:47 -0500

    change public methods to private methods

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -5,33 +5,41 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         int[] numArray = readIntegers();
-        System.out.println("numbers entered = " + Arrays.toString(numArray));
-        System.out.println("minimum number = " + findMin(numArray));
+        System.out.println("Numbers entered = " + Arrays.toString(numArray));
+        System.out.println("Minimum number = " + findMin(numArray));
     }
 
-    public static int[] readIntegers() {
+    private static int[] readIntegers() {
 
-        System.out.print("The number of integers : ");
+        System.out.print("The number of integers to compare : ");
         int count = scanner.nextInt();
+//        scanner.nextLine();
+
         int[] numbers = new int[count];
+
         for(int i = 0; i < count; i++) {
+            System.out.print("Enter No." + (i +1) + " : ");
             numbers[i] = scanner.nextInt();
+//            scanner.nextLine();
         }
+
         return numbers;
     }
 
-    public static int findMin(int[] array) {
+    private static int findMin(int[] array) {
         int min = array[0];
+
         for(int i = 1; i < array.length; i++) {
             if(min > array[i]) {
                 min = array[i];
             }
         }
+
         return min;
     }
 }
